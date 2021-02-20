4df97a92fa3a860591fd18c26e11db869ab8657f
Erwan Massou <erwan@netdevices.fr>
2021-02-20 16:42:12 +0100

    progress to deploy

--- back/server.js
+++ back/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import test from './Routes/testCode.js'
-import path, { dirname } from 'path'
+import path from 'path'
 
 const __dirname = path.resolve()
 
@@ -12,11 +12,12 @@ app.use(express.json())
 const port = process.env.PORT || 8000
 app.use('/api', test)
 
-  console.log(__dirname)
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/front/build')))
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'/front/build', 'index.html'))
   })
+}
 
 app.listen(port, () => {
   console.log('Server app listening on port ' + port)
