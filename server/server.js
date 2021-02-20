d41fc7064d20df1c6d29006cdb0b1a6c87b743e0
Erwan Massou <erwan@netdevices.fr>
2021-02-20 15:16:48 +0100

    try to resolve h10

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import test from './Routes/testCode.js'
 import path from 'path'
-import favicon from 'express-favicon'
 
 const __dirname = path.resolve()
 
@@ -14,7 +13,6 @@ const port = process.env.PORT || 8000
 app.use('/api', test)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(favicon(__dirname + '/build/favicon.ico'))
   app.use('/static', express.static(path.join(__dirname, '/front/build')))
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/front/build', 'index.html'))
