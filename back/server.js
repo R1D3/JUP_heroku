83d8329e2a15c1584d9b441126dc7f944785c1af
Erwan Massou <erwan@netdevices.fr>
2021-02-20 16:17:37 +0100

    resolve bug with res.send static file

--- back/server.js
+++ back/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import test from './Routes/testCode.js'
-import path from 'path'
+import path, { dirname } from 'path'
 
 const __dirname = path.resolve()
 
@@ -12,12 +12,11 @@ app.use(express.json())
 const port = process.env.PORT || 8000
 app.use('/api', test)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/static', express.static(path.join(__dirname, '/front/build')))
+  console.log(__dirname)
+  app.use(express.static(path.join(__dirname, '/front/build')))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/front/build', 'index.html'))
+    res.sendFile(path.join(__dirname,'/front/build', 'index.html'))
   })
-}
 
 app.listen(port, () => {
   console.log('Server app listening on port ' + port)
